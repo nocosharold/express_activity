ec93d85684e9ba3469144e4c9152541c2f36b5af
nocosharold <nocosharold@gmail.com>
2022-09-07 17:00:12 +0800

    Added Partials, Fonts, and Styling

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const path = require("path");
+const data = require("./data.json");
 
 const app = express();
-const port = 3333;
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, "/views"));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the homepage');
+    res.render('home');
 });
+
+app.get('*', (req, res) => {
+    res.render('notfound');
+})
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
